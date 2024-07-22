00a09470defbc6df1b89bca1d943b430db087329
TuanNguyen238 <tuannguyen23823@gmail.com>
2024-07-22 15:51:40 +0700

    fix user-routes

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const UserController = require("../controllers/user-controller");
+const UserController = require("../controller/user-controller.js");
 
 const router = express.Router();
 const userController = new UserController();
