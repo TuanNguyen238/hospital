0715c4be09e9456a458260c7b1983142e159a833
TuanNguyen238 <tuannguyen23823@gmail.com>
2024-07-22 20:55:56 +0700

    1

--- setup.js
+++ setup.js
@@ -4,21 +4,20 @@ const Role = require('./models/role.js');
 const { DataSource } = require('typeorm');
 
 class Setup {
-    
-    appDataSource = new DataSource({
-        type: 'mysql',
-        host: process.env.MYSQL_HOST,
-        port: process.env.MYSQL_PORT,
-        username: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-        synchronize: true, // Tự động đồng bộ hóa thực thể với cơ sở dữ liệu
-        logging: false,
-        entities: [User, Role],
-    });
 
     async setupDatabase() {
         try {
+            const appDataSource = new DataSource({
+                type: 'mysql',
+                host: process.env.MYSQL_HOST,
+                port: proce,
+                username: process.env.MYSQL_USER,
+                password: process.env.MYSQL_PASSWORD,
+                database: process.env.MYSQL_DATABASE,
+                synchronize: true, // Tự động đồng bộ hóa thực thể với cơ sở dữ liệu
+                logging: false,
+                entities: [User, Role],
+            });
             const userRepository = appDataSource.getRepository(User);
             const roleRepository = appDataSource.getRepository(Role);
             roleRepository.name = 'vcl';
