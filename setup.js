82145ef2b5b8e9b5a150b0b99a6681219ea04864
TuanNguyen238 <tuannguyen23823@gmail.com>
2024-07-23 09:03:26 +0700

    e

--- setup.js
+++ setup.js
@@ -1,26 +1,11 @@
 const AppDataSource = require('./utils/configs.js');
 const User = require('./models/user.js');
 const Role = require('./models/role.js');
-const {DataSource} = require('typeorm');
-
-
-const appDataSource = new DataSource({
-    type: 'mysql',
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT || 3306,
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    synchronize: true, // modify database
-    logging: false,
-    entities: ["../models/**/*.js"],
-});
 
 const setupDatabase = async () => {
     try {
-        await appDataSource.initialize();
-        const userRepository = appDataSource.getRepository(User);
-        const roleRepository = appDataSource.getRepository(Role);
+        const userRepository = AppDataSource.getRepository(User);
+        const roleRepository = AppDataSource.getRepository(Role);
         roleRepository.name = "12345";
         console.log("NAME: " + roleRepository.name);
         console.log("FIND: " + roleRepository.find());
