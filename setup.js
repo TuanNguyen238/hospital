1d4fbaee8cc58b5295c7d3c139db19c7e7119a4e
TuanNguyen238 <tuannguyen23823@gmail.com>
2024-07-23 10:44:35 +0700

    fix

--- setup.js
+++ setup.js
@@ -1,13 +1,11 @@
 const AppDataSource = require('./utils/configs.js');
 const User = require('./models/user.js');
 const Role = require('./models/role.js');
-const UserRepository = require('./repository/user-repository.js');
 
 class Setup {
-    #userRepository;
     async setupDatabase() {
         try {
-            this.#userRepository = new UserRepository();
+            const userRepository = AppDataSource.getRepository(User);
             const roleRepository = AppDataSource.getRepository(Role);
             
             let adminRole = await roleRepository.findOneBy({ name: 'admin' });
@@ -17,13 +15,13 @@ class Setup {
                 console.log('Admin role created.');
             }
     
-            const users = await this.#userRepository.find();
+            const users = await userRepository.find();
             if (users.length === 0) {
                 console.log('Users table does not exist or is empty. Seeding data...');
                 const usersData = [
                     { username: 'admin', email: 'tuannguyen23823@gmail.com', password: 'admin', phoneNumber: '0937837564', status: 'active', roles: [adminRole] }
                 ];
-                await this.#userRepository.save(usersData);
+                await userRepository.save(usersData);
                 console.log('Users seeded.');
             } else {
                 console.log('Users table already exists or is not empty.');
