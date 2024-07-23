3b3d08594d9f22e34aa0a44122adffa19b2aae4e
TuanNguyen238 <tuannguyen23823@gmail.com>
2024-07-23 09:08:36 +0700

    test

--- utils/configs.js
+++ utils/configs.js
@@ -12,7 +12,7 @@ const AppDataSource = new DataSource({
     database: process.env.MYSQL_DATABASE,
     synchronize: true, // modify database
     logging: false,
-    entities: [require('../models/*.js')],
+    entities: [require('../models/role.js'), require('../models/user.js')],
     //entities: [__dirname + '/../models/*.js']
 });
 
