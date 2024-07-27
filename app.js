f7101f9b61739f2332e8f88b3e95e8025b803f64
TuanNguyen238 <tuannguyen23823@gmail.com>
2024-07-27 09:25:24 +0700

    git add .

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const dotenv = require("dotenv");
 const userRoutes = require("./routes/user-routes.js");
 const authenticationRoutes = require("./routes/authentication-routes.js");
 const AppDataSource = require("./utils/configs.js");
+const admin = require("firebase-admin");
 
 dotenv.config();
+
+const serviceAccount = require("./service-account.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 AppDataSource.initialize()
   .then(() => {
@@ -21,6 +28,25 @@ AppDataSource.initialize()
     app.use(express.json());
     app.use("/users", userRoutes);
     app.use("/auth", authenticationRoutes);
+    app.post("/request-otp", async (req, res) => {
+      const { phoneNumber, fcmToken } = req.body;
+
+      const message = {
+        token: fcmToken,
+        data: {
+          phone_number: phoneNumber,
+          otp_request: "true",
+        },
+      };
+
+      try {
+        await admin.messaging().send(message);
+        res.send("OTP request sent to Flutter app");
+      } catch (error) {
+        console.error("Error sending FCM message:", error);
+        res.status(500).send("Error sending OTP request");
+      }
+    });
 
     app.listen(PORT, () => {
       console.log(`Server is running on PORT: ${PORT}`);
