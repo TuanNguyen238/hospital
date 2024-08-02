79e28dc6543ee1f76b457ff3e05444d1ff002345
TuanNguyen238 <tuannguyen23823@gmail.com>
2024-08-02 16:47:55 +0700

    add middleware

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const UserController = require("../controller/user-controller.js");
-const authenticateToken = require("../middleware/authMiddleware");
+const authenticateToken = require("../middleware/user-middleware.js");
 
 const router = express.Router();
 const userController = new UserController();
