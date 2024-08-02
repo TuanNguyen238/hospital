95d487f764ea9aab82be0fd7c46b3900992380ba
TuanNguyen238 <tuannguyen23823@gmail.com>
2024-08-02 16:51:38 +0700

    add middleware

--- middleware/user-middleware.js
+++ middleware/user-middleware.js
@@ -10,12 +10,12 @@ const authenticateToken = (req, res, next) => {
   const token = authHeader && authHeader.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ error: ErrorCode.TOKEN_UNAUTHENTICATED });
+    return res.status(500).json({ error: ErrorCode.TOKEN_UNAUTHENTICATED });
   }
 
   jwt.verify(token, SECRET_KEY, (err, user) => {
     if (err) {
-      return res.status(403).json({ error: ErrorCode.TOKEN_UNAUTHENTICATED });
+      return res.status(500).json({ error: ErrorCode.TOKEN_UNAUTHENTICATED });
     }
     next();
   });
