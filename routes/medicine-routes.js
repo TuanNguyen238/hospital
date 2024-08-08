47c7e8573ee370941f982b865f21ea331812a7be
TuanNguyen238 <tuannguyen23823@gmail.com>
2024-08-08 11:20:25 +0700

    .

--- routes/medicine-routes.js
+++ routes/medicine-routes.js
@@ -4,6 +4,6 @@ const MedicineController = require("../controller/medicine-controller.js");
 const router = express.Router();
 const medicineController = new MedicineController();
 
-router.post("/count", (req, res) => medicineController.getCount(req, res));
+router.get("/count", (req, res) => medicineController.getCount(req, res));
 
 module.exports = router;
