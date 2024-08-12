bb466e3ada1baccf2a8722f7381d9332a2835eca
TuanNguyen238 <tuannguyen23823@gmail.com>
2024-08-12 10:31:15 +0700

    .

--- routes/examRoom-routes.js
+++ routes/examRoom-routes.js
@@ -5,7 +5,7 @@ const ExamRoomcontroller = require("../controller/examRoom-controller.js");
 const router = express.Router();
 const examRoomcontroller = new ExamRoomcontroller();
 
-router.post("/create", UserMiddleware.authenticationTokenUser, (req, res) =>
-  examRoomcontroller.createPatient(req, res)
+router.post("/create", UserMiddleware.authenticationTokenDoctor, (req, res) =>
+  examRoomcontroller.createExamRoom(req, res)
 );
 module.exports = router;
