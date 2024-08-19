620478d710b8cda033b985edbeca5afe722fa306
TuanNguyen238 <tuannguyen23823@gmail.com>
2024-08-19 14:44:01 +0700

    .

--- models/medical-record.js
+++ models/medical-record.js
@@ -17,10 +17,12 @@ const MedicalRecord = new EntitySchema({
     diagnosis: {
       type: "varchar",
       length: 255,
+      nullable: true,
     },
     examResult: {
       type: "varchar",
       length: 255,
+      nullable: true,
     },
   },
   relations: {
