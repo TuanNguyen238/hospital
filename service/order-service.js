4521aa4d914befa7ceef9909c46fe7475551557f
TuanNguyen238 <tuannguyen23823@gmail.com>
2024-10-30 15:51:38 +0700

    .

--- service/order-service.js
+++ service/order-service.js
@@ -1,5 +1,6 @@
 const ErrorCode = require("../enum/error-code.js");
 const StatusCode = require("../enum/status-code.js");
+const MedicineRepository = require("../repository/medicine-repository.js");
 const OrderRepository = require("../repository/order-repository.js");
 const OrderMedicineRepository = require("../repository/orderMedicine-repository.js");
 const UserRepository = require("../repository/user-repository.js");
@@ -8,11 +9,13 @@ class OrderService {
   #orderRepository;
   #orderMedicineRepository;
   #userRepository;
+  #medicineRepository;
 
   constructor() {
     this.#orderRepository = new OrderRepository();
     this.#orderMedicineRepository = new OrderMedicineRepository();
     this.#userRepository = new UserRepository();
+    this.#medicineRepository = new MedicineRepository();
   }
 
   async createOrder(order) {
@@ -51,13 +54,18 @@ class OrderService {
     );
     console.log(doctor);
 
-    const newOrder = await this.#orderRepository.createEntity(client, doctor);
+    //const newOrder = await this.#orderRepository.createEntity(client, doctor);
 
-    const savedOrder = await this.#orderRepository.saveOrder(newOrder);
+    const savedOrder = await this.#orderRepository.saveOrder({
+      client: client,
+      doctor: doctor,
+      createAt: new Date(),
+    });
     for (const medicine of order.medicines) {
+      const medicineData = this.#medicineRepository.findById(medicine.id);
       await this.#orderMedicineRepository.saveOrderMedicine({
         orderId: savedOrder.id,
-        medicineId: medicine.medicineId,
+        medicineId: medicineData,
         quantity: medicine.quantity,
       });
     }
