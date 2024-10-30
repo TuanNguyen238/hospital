01bd55211afebf2bc1a80852f803fba90c77c2e5
TuanNguyen238 <tuannguyen23823@gmail.com>
2024-10-30 14:56:14 +0700

    .

--- routes/order-routes.js
+++ routes/order-routes.js
@@ -1,21 +1,15 @@
 const express = require("express");
 const UserMiddleware = require("../middleware/user-middleware.js");
+const OrderController = require("../controller/order-controller.js");
 
 const router = express.Router();
-const medicineController = new MedicineController();
+const orderController = new OrderController();
 
-router.get("/count", (req, res) => medicineController.getCount(req, res));
-router.post("/create", UserMiddleware.authenticateTokenAdmin, (req, res) =>
-  medicineController.createMedicine(req, res)
+router.post("/create", UserMiddleware.authenticationTokenDoctor, (req, res) =>
+  orderController.createOrder(req, res)
 );
-router.get("/", UserMiddleware.authenticateTokenAdmin, (req, res) =>
-  medicineController.getAllMedicine(req, res)
-);
-router.post("/delete", UserMiddleware.authenticateTokenAdmin, (req, res) =>
-  medicineController.deleteMedicine(req, res)
-);
-router.put("/update", UserMiddleware.authenticateTokenAdmin, (req, res) =>
-  medicineController.updateMedicine(req, res)
+router.get("/", UserMiddleware.authenticationTokenDoctor, (req, res) =>
+  orderController.getAllOrder(req, res)
 );
 
 module.exports = router;
