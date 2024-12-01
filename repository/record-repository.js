f8cfe98c38ec03420393fa6101c9e540ffd824f6
TuanNguyen238 <tuannguyen23823@gmail.com>
2024-12-01 21:24:53 +0700

    .

--- repository/record-repository.js
+++ repository/record-repository.js
@@ -46,11 +46,11 @@ class RecordRepository {
       const medicalRecords = await this.#repository
         .createQueryBuilder("medicalRecord")
         .leftJoinAndSelect("medicalRecord.patient", "patient")
-        // .leftJoinAndSelect("medicalRecord.prescription", "prescription")
-        // .leftJoinAndSelect("medicalRecord.detailedRecord", "detailedRecord")
-        // .leftJoinAndSelect("medicalRecord.examRoom", "examRoom")
-        // .leftJoinAndSelect("prescription.dosages", "dosages")
-        // .leftJoinAndSelect("dosages.medicine", "medicine")
+        .leftJoinAndSelect("medicalRecord.prescription", "prescription")
+        .leftJoinAndSelect("medicalRecord.detailedRecord", "detailedRecord")
+        .leftJoinAndSelect("medicalRecord.examRoom", "examRoom")
+        .leftJoinAndSelect("prescription.dosages", "dosages")
+        .leftJoinAndSelect("dosages.medicine", "medicine")
         .where("patient.patientCode = :patientCode", { patientCode })
         .getMany();
 
